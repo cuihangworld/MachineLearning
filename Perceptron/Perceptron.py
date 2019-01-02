54a8eca11ec975d4e7cc92011f028804623f3a7a
Nikou <15663405528m@sina.cn>
2019-01-02 22:37:51 +0800

    add
    Perceptron input done
    Perceptron GramMat done

--- Perceptron/Perceptron.py
+++ Perceptron/Perceptron.py
@@ -1,8 +1,7 @@
 import numpy as np
 
-TrainData = []
-TrainDataMat = []
-def _inputTrainData(path):
+
+def _inputTrainData(path, TrainData):
     with open(path, 'r') as fileReader:
         while True:
             line = fileReader.readline()
@@ -13,15 +12,25 @@ def _inputTrainData(path):
             row = line.split()
             rowInt = [int(i) for i in row]
             TrainData.append(rowInt)
+            TrainDataMat = np.array(TrainData)
 
-def _CalGramMat():
-    TrainDataMat = np.mat(TrainData)
+    return TrainDataMat
+
+def _CalGramMat(TrainDataMat):
+    #delete Mat's the last col which is label
+    TrainDataMat = np.delete(TrainDataMat, -1, axis = 1)
+    TrainDataMatT = np.transpose(TrainDataMat)
+    TrainDataMatMul = np.dot(TrainDataMat, TrainDataMatT)
+
+    return TrainDataMatMul
 
 #def _train():
 
 if __name__ == '__main__':
     path = "train.txt"
-    _inputTrainData(path)
-    _CalGramMat()
-    print (TrainData)
+    TrainData = []
+    TrainDataMat = _inputTrainData(path, TrainData)
+    TrainDataMatMul = _CalGramMat(TrainDataMat)
+
     print (TrainDataMat)
+    print(TrainDataMatMul)

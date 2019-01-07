8bdf05df0cf9a15fa66008d7137014f600169a37
Nikou <15663405528m@sina.cn>
2019-01-07 23:04:22 +0800

    create kdtree

--- K-NN/Knn.py
+++ K-NN/Knn.py
@@ -4,13 +4,35 @@ import sys
 sys.path.append('E:\机器视觉\机器学习\MachineLearning\DataFeed')
 import InputData
 
-# create kd-tree, the input is x and label y
-def _createKdTree(DataXYMat):
-    # sort mat depend on the first col
-    DataXYMat = DataXYMat[DataXYMat[:,0].argsort()]
+class Node(object):
+    def __init__(self, DataMat, Left, Right):
+        self.left = Left
+        self.right = Right
+        self.datamat = DataMat
 
+# create kd-tree, the input is x and label y
+def _createKdTree(DataXYMat, SortCol):
+    # sort mat depend on the first col
+    DataXYMat = DataXYMat[DataXYMat[:,SortCol].argsort()]
+    MidPointIndex = int(len(DataXYMat)/2 + 1)
+    NULL = [0 for i in DataXYMat[0]]
+    node = Node(DataXYMat[MidPointIndex - 1], NULL, NULL)
+    leftnodes = DataXYMat[:MidPointIndex-1]
+    rightnodes = DataXYMat[MidPointIndex:]
+    SortCol = SortCol + 1
+    if 1 == len(DataXYMat):
+        return node
+    elif 2 == len(DataXYMat):
+        node.left = _createKdTree(leftnodes, SortCol)
+    else:
+        node.left = _createKdTree(leftnodes, SortCol)
+        node.right = _createKdTree(rightnodes, SortCol)
+
+    return node
 
 if __name__ == '__main__':
     path = "train.txt"
     TrainDataMat = InputData._inputData(path)
-    _createKdTree(TrainDataMat)
+    node = _createKdTree(TrainDataMat, 0)
+
+    print (node.right.datamat)

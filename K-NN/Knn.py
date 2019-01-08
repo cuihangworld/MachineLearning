6c31da6657962cefced73541eb3ebe72de63e3e4
Nikou <15663405528m@sina.cn>
2019-01-08 21:31:59 +0800

    kdtreesearch
    can't run: object of class can't be func's arg

--- K-NN/Knn.py
+++ K-NN/Knn.py
@@ -30,9 +30,91 @@ def _createKdTree(DataXYMat, SortCol):
 
     return node
 
+# select nearest node, Xn is feature
+def _SelectKdTree(NodeTree, DataSelected, Xn, NearestLenth):
+    if 0 == NodeTree.left.datamat[-1] and 0 == NodeTree.right.datamat[-1]:
+        NodeTreeX = np.delete(NodeTree.datamat, -1, axis = 1)
+        #DataSelectedX = np.delete(DataSelected, -1, axis = 1)
+        Lenth = np.sqrt(np.sum(np.square(NodeTreeX - DataSelected)))
+        if Lenth < NearestLenth:
+            NearestLenth = Lenth
+            NearestNode  = NodeTree.datamat
+        return NearestNode, NearestLenth
+    # into left
+    elif DataSelected[Xn] < NodeTree.datamat[Xn]:
+        # left is null
+        if 0 == NodeTree.left.datamat[-1]:
+            NodeTreeX = np.delete(NodeTree.datamat, -1, axis=1)
+            #DataSelectedX = np.delete(DataSelected, -1, axis=1)
+            Lenth = np.sqrt(np.sum(np.square(NodeTreeX - DataSelected)))
+            if Lenth < NearestLenth:
+                NearestLenth = Lenth
+                NearestNode = NodeTree.datamat
+            return NearestNode, NearestLenth
+        Xn = Xn - 1
+        NearestNode, NearestLenth = _SelectKdTree(NodeTree.left, DataSelected, Xn, NearestLenth)
+        NodeTreeX = np.delete(NodeTree.datamat, -1, axis=1)
+        #DataSelectedX = np.delete(DataSelected, -1, axis=1)
+        Lenth = np.sqrt(np.sum(np.square(NodeTreeX - DataSelected)))
+        if Lenth < NearestLenth:
+            NearestLenth = Lenth
+            NearestNode  = NodeTree.datamat
+        # using NearestLenth as R draw circle judge whether cover NodeTree's right
+        if 0 == NodeTree.right.datamat[-1]:
+            # right is null
+            return NearestNode, NearestLenth
+        else:
+            if (DataSelected[Xn] + NearestLenth) < NodeTree[Xn]:
+                # uncover
+                return NearestNode, NearestLenth
+
+            Xn = Xn - 1
+            NearestNode, NearestLenth = _SelectKdTree(NodeTree.right, DataSelected, Xn, NearestLenth)
+            return NearestNode, NearestLenth
+
+    elif DataSelected[Xn] > NodeTree.datamat[Xn]:
+        # right is null
+        if 0 == NodeTree.right.datamat[-1]:
+            NodeTreeX = np.delete(NodeTree.datamat, -1, axis=1)
+            #DataSelectedX = np.delete(DataSelected, -1, axis=1)
+            Lenth = np.sqrt(np.sum(np.square(NodeTreeX - DataSelected)))
+            if Lenth < NearestLenth:
+                NearestLenth = Lenth
+                NearestNode = NodeTree.datamat
+            return NearestNode, NearestLenth
+        Xn = Xn - 1
+        NearestNode, NearestLenth = _SelectKdTree(NodeTree.right, DataSelected, Xn, NearestLenth)
+        NodeTreeX = np.delete(NodeTree.datamat, -1, axis=1)
+        #DataSelectedX = np.delete(DataSelected, -1, axis=1)
+        Lenth = np.sqrt(np.sum(np.square(NodeTreeX - DataSelected)))
+        if Lenth < NearestLenth:
+            NearestLenth = Lenth
+            NearestNode = NodeTree.datamat
+        # using NearestLenth as R draw circle judge whether cover NodeTree's right
+        if 0 == NodeTree.left.datamat[-1]:
+            # right is null
+            return NearestNode, NearestLenth
+        else:
+            if (DataSelected[Xn] + NearestLenth) < NodeTree[Xn]:
+                # uncover
+                return NearestNode, NearestLenth
+
+            Xn = Xn - 1
+            NearestNode, NearestLenth = _SelectKdTree(NodeTree.left, DataSelected, Xn, NearestLenth)
+            return NearestNode, NearestLenth
+
+
+
+
 if __name__ == '__main__':
     path = "train.txt"
     TrainDataMat = InputData._inputData(path)
-    node = _createKdTree(TrainDataMat, 0)
 
-    print (node.right.datamat)
+    node = _createKdTree(TrainDataMat, 0)
+    DataSelected = np.array([5.5, 3.5])
+    Xn = 0
+    NearestLenth = 100000
+
+    NearestNode, NearestLenth = _SelectKdTree(, DataSelected, Xn, NearestLenth)
+    print (NearestNode)
+    print (NearestLenth)
